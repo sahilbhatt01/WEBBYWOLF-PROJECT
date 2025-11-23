de446e7683be37755ce8894365bed6db77bc4467
sahilbhatt01 <sahilbhatt540@gmail.com>
2025-11-23 21:16:03 +0530

    WEBBYWOLF FIRST  commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+theme: {
+extend: {
+colors: {
+primary: "#1959AC",
+secondary: "#0546D2",
+textDark: "#222222",
+},
+fontFamily: {
+heading: ["Roboto Condensed", "sans-serif"],
+inter: ["Inter", "sans-serif"],
+},
+},
+},
+};
